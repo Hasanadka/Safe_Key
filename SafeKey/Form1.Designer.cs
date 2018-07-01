d16f914402eb96a55271687887cd7f20145780e1
Sagar Hasanadka <sagarhasanadka@gmail.com>
2018-07-02 02:16:10 +0530

    Add files via upload

---
+++ SafeKey/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SafeKey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BtnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(68, 103);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(221, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(68, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(221, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // BtnCopy
+            // 
+            this.BtnCopy.Location = new System.Drawing.Point(141, 156);
+            this.BtnCopy.Name = "BtnCopy";
+            this.BtnCopy.Size = new System.Drawing.Size(75, 23);
+            this.BtnCopy.TabIndex = 2;
+            this.BtnCopy.Text = "Copy";
+            this.BtnCopy.UseVisualStyleBackColor = true;
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 259);
+            this.Controls.Add(this.BtnCopy);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnCopy;
+    }
+}
+
---
+++ SafeKey/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+namespace SafeKey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            t_Tick(null, null);
+            KeyGenerator();
+            
+        }
+       
+        Timer t = new Timer();
+        Timer t2 = new Timer();
+        Random rnd = new Random();
+
+        private void KeyGenerator()
+        {
+
+            t.Start(); 
+            t.Interval = 1000 * 20; 
+            t.Tick += new EventHandler(this.t_Tick);
+            t2.Interval = 200;
+            t2.Tick += new EventHandler(this.timer1_Tick);
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            t2.Start();
+            RandomCreate();
+            
+            progressBar1.Value = 0;
+            progressBar1.Maximum = 100;
+            
+        }
+
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value != progressBar1.Maximum)
+                progressBar1.Increment(1);
+            else
+            {
+                t2.Stop();
+            }
+           
+        }
+
+
+        void RandomCreate()
+        {
+            string input = "Sagar@tcs.com";
+            char[] values = input.ToCharArray();
+            char[] delimiter = {'-',':','/',' '};
+            string hexstring = string.Empty;
+            string hexOutput = string.Empty;
+            string hexFormat = string.Empty;
+            string[] Interval;
+            int value;
+            string timestring = string.Empty;
+            BigInteger Output = 0;
+            foreach (char letter in values)
+            {
+                
+                value = Convert.ToInt32(letter);
+                
+                hexOutput = String.Format("{0:X}", value);
+                hexFormat= Convert.ToString(Convert.ToInt32(hexOutput, 16), 8);
+                hexstring = hexstring + (hexFormat.Length == 2 ? "0" + hexFormat : hexFormat);
+            }
+            Interval = new string[8];
+            Interval=DateTime.UtcNow.ToString().Split(delimiter);
+            //Interval[5] = (Convert.ToInt32(Interval[5]) < 30 ? "17" : "47");
+            if (Convert.ToInt32(Interval[5]) < 20)
+                Interval[5] = "17";
+            else if (Convert.ToInt32(Interval[5]) < 40)
+                Interval[5] = "37";
+            else 
+                Interval[5] = "57";
+            timestring = (Convert.ToInt32(Interval[4] + Interval[5] + Interval[3]) * Convert.ToInt32(Interval[4] + Interval[5])).ToString() + Interval[5];
+            
+            while (hexstring.Length > 12)
+            {
+                BigInteger var = 0;
+                var = BigInteger.Parse(hexstring) / 12;
+                hexstring = var.ToString();
+            }
+
+            while (hexstring.Length < 12)
+            {
+                hexstring = (BigInteger.Parse(hexstring)*3).ToString();
+            }
+
+            hexOutput = (((BigInteger.Parse(hexstring + Interval[5]) / BigInteger.Parse(timestring)) * Convert.ToInt32(Interval[4] + Interval[5])) / Convert.ToInt32(Interval[5] + Interval[4])).ToString();
+
+            while (hexOutput.Length < 6)
+            {
+                Output = BigInteger.Parse(hexOutput)<< 1;
+                hexOutput = Output.ToString();
+            }
+
+            textBox1.Text = hexOutput;
+        }
+    }
+}
